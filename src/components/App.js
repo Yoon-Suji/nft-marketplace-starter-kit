c4b861026070bf2893e9ca6cb94b3d17d34b50d2
Yoon-Suji <jidl88@ewhain.net>
2022-03-06 00:51:32 +0900

    Add: Section23 NFT Front End II

--- src/components/App.js
+++ src/components/App.js
@@ -31,7 +31,7 @@ class App extends Component {
     async loadBlockchainData() {
         const web3 = window.web3
         const accounts = await web3.eth.getAccounts()
-        this.setState({account:accounts})
+        this.setState({account:accounts[0]})
 
         // create a constant js variable networkId which is set to blockchain networkId
         const networkId = await web3.eth.net.getId()
@@ -40,21 +40,51 @@ class App extends Component {
             const abi = KryptoBird.abi
             const address = networkData.address
             const contract = new web3.eth.Contract(abi, address)
-            console.log(contract)
+            this.setState({contract})
 
+            // call the total supply of our KryptoBird
+            const totalSupply = await contract.methods.totalSupply().call()
+            this.setState({totalSupply})
+
+            // set up array to keep track of tokens
+            // load KryptoBirdz
+            for (let i = 1; i <= totalSupply; i++) {
+                const KryptoBird = await contract.methods.kryptoBirdz(i-1).call()
+                this.setState({
+                    kryptoBirdz: [...this.state.kryptoBirdz, KryptoBird] // wow.. 
+                })
+            }
+
+        } else {
+            window.alert('Smart contract not deployed')
         }
+    }
+
+    // with minting we are sending information and need to specify the account
+    mint = (kryptoBird) => {
+        this.state.contract.methods.mint(kryptoBird).send({from:this.state.account})
+        .once('receipt', (receipt) => {
+            this.setState({
+                kryptoBirdz: [...this.state.kryptoBirdz, KryptoBird]
+            })
+        })
+
     }
 
     constructor(props) {
         super(props);
         this.state = {
-            account: ''
+            account: '',
+            contract: null,
+            totalSupply: 0,
+            kryptoBirdz: []
         }
     }
 
     render() {
         return (
             <div>
+                {console.log(this.state.kryptoBirdz)}
                 <nav className='navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow'>
                     <div className='navbar-brand col-sm-3 col-md-3 mr-0' style={{color:'white'}}>
                         Krypto Birdz NFTs (Non Fungible Tokens)
@@ -66,6 +96,25 @@ class App extends Component {
                         </small>
                     </ul>
                 </nav>
+                <div className="container-fluid mt-1">
+                    <div className='row'>
+                        <main role='main' className="col-lg-12 d-flx text-center">
+                            <div className='container mr-auto ml-auto' style={{opacity:'0.8'}}>
+                                <h1 style={{color:'white'}}>KryptoBirdz - NFT Marketplace</h1>
+                                <form onSubmit={(event)=>{
+                                    event.preventDefault()
+                                    const kryptoBird = this.kryptoBird.value
+                                    this.mint(kryptoBird)
+                                }}>
+                                    <input type='text' placeholder='Add a file location' className='mb-1' 
+                                    ref={(input)=>this.kryptoBird = input}/>
+                                    <input style={{margin:'6px'}}type='submit' className='btn btn-primary btn-black' value='MINT'/>
+
+                                </form>
+                            </div>
+                        </main>
+                    </div>
+                </div>
             </div>
         )
     }
